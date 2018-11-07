28f86150c9ba28de37e930eb59fe6e6c03622229
danhpham <danhpham312@gmail.com>
2018-11-06 16:23:03 -0800

    UPDATED FAKERECORD, DATA POPULATED

--- project/src/components/Userpage.js
+++ project/src/components/Userpage.js
@@ -6,12 +6,14 @@ import {
 } from './firebase/firebase';
 import './css/user.css';
 import {Redirect } from 'react-router-dom';
-import { MDBDataTable } from 'mdbreact';
+import { MDBBtn, MDBTable, MDBTableBody, MDBTableHead  } from 'mdbreact';
+
 class UserPage extends Component{
   constructor(props) {
     super(props);
     this.state={
         users:[],
+        goldpackets:[],
       gold:false,
       silver:false,
       redirect:false
@@ -31,25 +33,32 @@ class UserPage extends Component{
 
   componentDidMount() {
 
+
+
+
+
     var user = auth.currentUser;
     if(user){
 
-     const userref = realdb.ref().child('users');
-
-     userref.once("value", snap => {
+     const goldref = realdb.ref().child('Gold/Fight');
+     goldref.once("value", snap => {
          // Handle state
-         let listusers = []
+         let listFight = []
          snap.forEach(child => {
-           let user ={
+           let tickets ={
 
-             email:child.val().email,
-             fullname:child.val().fullname,
+             name:child.val().name,
+             Stars:child.val().Stars,
+             TicketStatus:child.val().TicketStatus,
+             Price:child.val().Price,
+             contacts:child.val().contacts,
+
              id: child.val().id
 
            }
-             listusers.push(user.email);
+             listFight.push(tickets.name);
          });
-         this.setState({users: listusers})
+         this.setState({goldpackets: listFight})
      });
 
    }
@@ -60,16 +69,11 @@ class UserPage extends Component{
    }
                 }
   render() {
-
-
+    const listofFight = this.state.goldpackets.map((data,i) => <tr key={i}>{data}</tr>);
     if(this.state.redirect){
       return (<Redirect to={'/Logining'}/>);
     }
 
-
-    const listofUsers = { columns: this.state.users };
-
-    if(this.state.gold){
       return (
 
         <div>
@@ -109,73 +113,49 @@ class UserPage extends Component{
 
 
       </div>
-
           <div className="flex-user">
+          {
+            this.state.gold || this.state.silver
+        ?(
 
-          <MDBDataTable   striped
-       bordered
-       hover
-          data={listofUsers} />
+          <table id="dtBasicExample" >
+            <thead>
+              <tr>
+                <th className="th-bg">Tickets Name
+                  <i className="fa fa-sort float-right" aria-hidden="true"></i>
+                </th>
+
+              </tr>
+            </thead>
+
+            {
+              this.state.gold
+              ?
+              (<tbody>{listofFight}</tbody>)
+              :null
+            }
 
 
 
+            <tfoot>
+              <tr>
+                <th>Email
+                </th>
 
-
+              </tr>
+            </tfoot>
+          </table>
+        )
+        :null
+}
         </div>
           </div>
         </div>
 
        )
     }
-      return (
-
-        <div>
-        <div className="user-page">
-        <div className="flex-box">
-
-        <div className="inner-boxa">
-        <div>
-        <p> Gold </p>
-
-        <li> Contains:  </li>
-        <li> Business Flight Tickets </li>
-        <li> Four Stars Hotel Up </li>
-        <li> Premium Car Rental </li>
-
-        <div className="inner-a" >
-        <button  onClick={this.GoldPackets}> Get Gold Packets </button>
-        </div>
-        </div>
-        </div>
-        <div className="inner-boxb">
-
-        <div>
-        <p> Silver </p>
-
-        <li> Contains:  </li>
-        <li> Economy Flight Tickets </li>
-        <li> Four Stars Hotel Down </li>
-        <li> Regular Car Rental </li>
-
-        <div className="inner-b" >
-        <button onClick={this.SilverPackets} > Get Silver Packets </button>
-        </div>
-        </div>
-          </div>
 
 
-
-      </div>
-
-          <div className="flex-user">
-
-
-
-        </div>
-          </div>
-        </div>
-);
-}
 }
 
 
